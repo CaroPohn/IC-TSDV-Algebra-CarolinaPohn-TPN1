4799b506763288c86d70b7b981e1556604c6965e
juandigilio <juanignaciodigilio@gmail.com>
2023-09-23 17:51:08 -0300

    Done
    Done

---
+++ Tp1_Algebra (Colisiones con poligonos)/Tp1_Algebra (Colisiones con poligonos)/Main.cpp
@@ -0,0 +1,103 @@
+#include <raylib.h>
+#include <vector>
+
+using namespace std;
+
+
+bool PointInPolygon(Vector2 ballPosition, Vector2 vertices[], int totalVertices);
+
+void main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Dibujar Polígonos y Mover Pelota");
+
+    Vector2 ballPosition = { static_cast<float>(screenWidth) / 2, static_cast<float>(screenHeight) / 2 };
+    Vector2 vertices[50];
+    int totalVertices = 0;
+    Vector2 ballSpeed = { 5.0f, 5.0f };
+    int ballRadius = 20;
+    Color ballColor = RED;
+
+    vector<Vector2> polygonPoints;
+    bool isDrawingPolygon = false;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        // Update
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += ballSpeed.x;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= ballSpeed.x;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += ballSpeed.y;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= ballSpeed.y;
+
+        // Check if ball is inside any polygon
+        bool insidePolygon = false;
+
+        if (totalVertices >= 3)  // Check if there are enough vertices to form a polygon
+        {
+            insidePolygon = PointInPolygon(ballPosition, vertices, totalVertices);
+        }
+
+        if (insidePolygon)
+            ballColor = GREEN;
+        else
+            ballColor = RED;
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            isDrawingPolygon = true;
+            vertices[totalVertices].y = GetMousePosition().y;
+            vertices[totalVertices].x = GetMousePosition().x;
+            totalVertices++;
+        }
+        else if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))
+        {
+            isDrawingPolygon = false;
+        }
+
+        if (totalVertices > 1)
+        {
+            for (int i = 0; i < totalVertices - 1; i++)
+            {
+                DrawLineV(vertices[i], vertices[i + 1], RED);
+            }
+
+            if (isDrawingPolygon)
+            {
+                DrawLineEx(vertices[totalVertices - 1], GetMousePosition(), 2, BLACK);
+            }
+        }
+
+        DrawCircleV(ballPosition, ballRadius, ballColor);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    //return 0;
+}
+
+
+bool PointInPolygon(Vector2 ballPosition, Vector2 vertices[], int totalVertices)
+{
+    int crossings = 0;
+
+    for (int i = 0, j = totalVertices - 1; i < totalVertices; j = i++)
+    {
+        if (((vertices[i].y > ballPosition.y) != (vertices[j].y > ballPosition.y)) &&
+            (ballPosition.x < (vertices[j].x - vertices[i].x) * (ballPosition.y - vertices[i].y) / (vertices[j].y - vertices[i].y) + vertices[i].x))
+        {
+            crossings++;
+        }
+    }
+
+    return (crossings % 2 == 1);
+}
